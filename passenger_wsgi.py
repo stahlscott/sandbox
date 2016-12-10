cee88937b7b496e347d6dc615dd704cc62beb1e1
stahlscott <stahl.scott@gmail.com>
2016-12-10 09:07:37 -0500

    Static files, basic formatting

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,13 +4,14 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('template.html',
+                           my_string="Wheeeee!",
+                           my_list=[0, 1, 2, 3, 4, 5])
 
 
 @application.route('/template')
 def basic_jinja():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
-
+    pass
 
 if __name__ == "__main__":
     application.run()
