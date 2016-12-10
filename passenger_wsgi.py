afa74eeb327820df4d8a682c9d18ed33c4d3c368
stahlscott <stahl.scott@gmail.com>
2016-12-10 06:50:35 -0500

    Basic jinja2 templating

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route('/template')
+def basic_jinja():
+    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
 
 if __name__ == "__main__":
     application.run()
